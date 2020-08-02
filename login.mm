71c95776c231503bde39a9725bb2a3bb2415d38a
Tuwani <vtuwani@informatica.com>
2020-08-02 13:25:25 +0530

    adding mind map and bugs doc

---
+++ login.mm
@@ -0,0 +1,17 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1596353904204" ID="ID_496891519" MODIFIED="1596353911471" TEXT="login">
+<node CREATED="1596353914943" ID="ID_885265618" MODIFIED="1596353916838" POSITION="right" TEXT="P1 ">
+<node CREATED="1596353918510" ID="ID_953351530" MODIFIED="1596353933184" TEXT="check login with correct and incorrect credentials"/>
+<node CREATED="1596353935702" ID="ID_1209693727" MODIFIED="1596353951154" TEXT="user token to get phone and search record using phone"/>
+<node CREATED="1596353953599" ID="ID_1517758440" MODIFIED="1596353976293" TEXT="get all the records using all the phone numbers"/>
+</node>
+<node CREATED="1596353979133" ID="ID_916595198" MODIFIED="1596353983275" POSITION="left" TEXT="P2">
+<node CREATED="1596353985023" ID="ID_1662570296" MODIFIED="1596354076080" TEXT="try to get incorrect phone number and check error(BUG 1)"/>
+</node>
+<node CREATED="1596354027119" ID="ID_1920131977" MODIFIED="1596354036799" POSITION="right" TEXT="parallel cases">
+<node CREATED="1596354039088" ID="ID_1409576255" MODIFIED="1596354064640" TEXT="try to execute test in Parallel to check how much load the server is able to take"/>
+</node>
+<node CREATED="1596354771175" ID="ID_26514920" MODIFIED="1596354788324" POSITION="left" TEXT="tests for phone number details validations can also be added"/>
+</node>
+</map>
